71a75a672f9bde0c179f4a4b97ea90b036d193e3
samuelrm5 <srodriguez438@misena.edu.co>
2022-06-07 21:55:55 -0500

    actualizacion registro_mascotas 7 Junio

--- src/components/pages/Registro_mascotas/Registro_mascotas.jsx
+++ src/components/pages/Registro_mascotas/Registro_mascotas.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { ButtonUI } from '../../UI/ButtonUI/ButtonUI'
-import { ImagenUI } from '../../UI/ImagenUI/ImagenUI'
 import { InputUI } from '../../UI/InputUI/InputUI'
-import { BsGenderMale, BsGenderFemale, BsPatchQuestion } from 'react-icons/bs';
+import { TbGenderMale, TbGenderFemale } from 'react-icons/tb';
+import { FaInfoCircle } from 'react-icons/fa';
 import './Registro_mascotas.css'
 import imagePerro from './img/perro.png'
 import imageGato from './img/gato.png'
 import imageLupa from './img/lupa.png'
+import imageLoro from './img/loro.png'
 import imageOtro from './img/varios.png'
+import titulo from './img/titulo.png'
 
 
 
@@ -25,6 +27,8 @@ export const Registro_mascotas = () => {
         }
         else if ( target.value === "gato" ) {
             setType_pet( imageGato )
+        } else if ( target.value === "loro") {
+            setType_pet( imageLoro )
         } else if ( target.value === "otro") {
             setType_pet( imageOtro )
         } else {
@@ -47,21 +51,50 @@ export const Registro_mascotas = () => {
 
     }
 
+    const handleFcInfo = () => {
+        console.log("ENTRA");
+        const recomendacion = document.getElementById('recomendacion_registro_mascotas');
+        const fcInfo = document.getElementById('FcInfo');
+
+        fcInfo.classList.add('deshabilitar')
+
+        
+        recomendacion.classList.remove('animate__backOutRight');
+        recomendacion.classList.remove('ocultarElemento');
+        recomendacion.classList.add('animate__fadeInRight');
+        recomendacion.classList.add('mostrar_elemento');
+
+        setTimeout( () => {
+            
+            recomendacion.classList.remove('animate__fadeInRight');
+            recomendacion.classList.add('animate__backOutRight');
+        }, 9470);
+        setTimeout( () => {
+
+            recomendacion.classList.remove('mostrar_elemento');
+            recomendacion.classList.add('ocultarElemento');
+            fcInfo.classList.remove('deshabilitar');
+        }, 10000);
+
+
+    }
+
     return (
-        <div className="loginContainer">
+        <div className="loginContainer animate__animated animate__fadeIn">
             <div className="login_cont_iz animate__animated animate__fadeInLeftBig">
                 <img id='type_animal' src={type_pet} alt="" />
             </div>
             <div className='login_cont_dr'>
-                <ImagenUI />
+                <img src={titulo} className='img_registro_mascotas' />
                 <div className="loginData registro_mascota">
-                    <p>Registro Mascotas</p>
+                    <p><b>¡</b> Registra tu mascota <b>!</b></p>
                     <div className='div_registro_mascotas_select'>
                         <p id='p_registro_mascotas'>Tipo</p>
-                        <select onChange={ handlePet } name="pet" id="pet">
-                            <option selected disabled>Selecciona tu tipo de mascota</option>
+                        <select onChange={ handlePet } name="type" id="select_type">
+                            <option id="option_disabled">Selecciona el tipo de mascota</option>
                             <option value="perro">Perro</option>
                             <option value="gato">Gato</option>
+                            <option value="loro">Ave</option>
                             <option value="otro">Otro</option>
                         </select>
                     </div>
@@ -71,11 +104,20 @@ export const Registro_mascotas = () => {
                         style = 'inputLogin'
                         txt = 'Nombre mascota'
                     />
-                    <InputUI 
-                        type='text'
-                        style = 'inputLogin'
-                        txt = 'ID '
-                    />
+                    <div className="div_registro_mascotas_input">
+                        <InputUI 
+                            type='text'
+                            style = 'inputLogin'
+                            txt = 'ID '
+                        />
+                        <p id='recomendacion_registro_mascotas' className='ocultar_elemento animate__animated'>
+                            <b>sugerencia:</b> el ID de tu mascota podría ser tu documento y el nombre de tu mascota. ej.18xxxxfirulais
+                        </p>
+                        <div>
+                            <a onClick={handleFcInfo} id="FcInfo"><FaInfoCircle /></a>
+                        </div>
+                        
+                    </div>
                     <div className='div_registro_mascotas_input'>
                         <InputUI 
                             type='text'
@@ -92,14 +134,14 @@ export const Registro_mascotas = () => {
                     <div className='div_registro_mascotas_select'>
                         {
                             (gender === "macho")
-                            ? <p id='p_registro_mascotas'>Sexo <span id='genderMale'><BsGenderMale /></span></p>
+                            ? <p id='p_registro_mascotas'>Sexo <span id='genderMale'><TbGenderMale /></span></p>
                             : ( gender === "hembra") 
-                            ? <p id='p_registro_mascotas'>Sexo <span id='genderFemale'><BsGenderFemale /></span></p>
-                            : <p id='p_registro_mascotas'>Sexo <span><BsPatchQuestion /></span></p>
+                            ? <p id='p_registro_mascotas'>Sexo <span id='genderFemale'><TbGenderFemale /></span></p>
+                            : <p id='p_registro_mascotas'>Sexo</p>
                         }
                         
-                        <select onChange={ handleGenderPet } name="pet" id="pet">
-                            <option selected disabled>Selecciona el sexo</option>
+                        <select onChange={ handleGenderPet } name="gender" id="select_gender">
+                            <option id='option_disabled'>Selecciona el sexo</option>
                             <option value="macho">Macho</option>
                             <option value="hembra">Hembra</option>
                         </select>
