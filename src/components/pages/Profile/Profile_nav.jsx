57d1a01e6700837b4668adea2487155c51840f2c
JuanVera326 <jjvera86@misena.edu.co>
2022-06-08 13:28:52 -0500

    Instancia Profile

---
+++ src/components/pages/Profile/Profile_nav.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Profile/'
+
+export const Profile_nav = () => {
+  return (
+    <div>Profile_nav</div>
+  )
+}
---
+++ src/components/pages/Profile/Profile_photo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Profile_photo = () => {
+  return (
+    <div>Profile_photo</div>
+  )
+}
