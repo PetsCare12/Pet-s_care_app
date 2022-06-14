4ee8412bb91e339e406d16c2980dfd06ca7d4976
JuanVera326 <jjvera86@misena.edu.co>
2022-06-14 10:39:52 -0500

    Login en terminacion

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import { Profile_nav } from "../Profile/Profile_nav";
 import { Profile_photo } from "./Profile_photo";
 import './Profile.css'
+import { CardsPets } from '../../Layouts/ContPets/CardsPets';
+import { ButtonUI } from "../../UI/ButtonUI/ButtonUI";
+import { useState,useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { ImagenUI } from '../../UI/ImagenUI/ImagenUI';
+import { pets_images } from '../../../helpers/Pets_care_images';
+
 
 export const Profile = () => {
+  
+  const [pet, setPet] = useState([]) //Consulta de mascotas a renderizar
+  
+  const printPets = () => {}  //fetch EndPoint consulta de mascotas
+  
+  useEffect(() => {}, []) //Iniciar renderizado
+  
+  let testArr = ["pepe","cabesiamarillo","verde",3,"no","loro"]
+
+  console.log(testArr);
   return (
     <div className='profile'>
         <div className='section_profile_left'>
@@ -11,7 +28,11 @@ export const Profile = () => {
             <Profile_nav />
         </div>
         <div className='section_profile_right'>
-          <h1>Mis Mascotas</h1>
+          <h1 className='titlePage title'>Mis Mascotas</h1>
+          <Link to="/registro_mascotas" className='a am'><ButtonUI event={printPets} text="Agregar Mascota" style="btnLoginCrear btnAgregarMascota"></ButtonUI></Link>
+          <br />
+          <hr />
+          <CardsPets text="Mascotas aqui..." arr={testArr}></CardsPets> {/* Renderizado de las mascotas   relacionadas */}
         </div>
     </div>
   )
--- src/components/pages/Profile/Profile_nav.jsx
+++ src/components/pages/Profile/Profile_nav.jsx
@@ -1,19 +1,82 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './Profile.css'
+import { useRef } from 'react'
+import { Link } from 'react-router-dom'
+import { GrDocumentUpdate } from "react-icons/gr";
+import { IoMdPaper } from "react-icons/io";
+import { MdDateRange } from "react-icons/md";
 
 export const Profile_nav = () => {
+
+  // const li1 = useRef(null);
+  // const li2 = useRef(null);
+  // const li3 = useRef(null);
+  const li4 = useRef(null);
+  const li5 = useRef(null);
+  const li6 = useRef(null);
+  // const li7 = useRef(null);
+  // const li8 = useRef(null);
+
+  const cleanCLass = () => {
+      // li8.current.classList.remove('active_li');
+      // li7.current.classList.remove('active_li');
+      li6.current.classList.remove('active_li');
+      li5.current.classList.remove('active_li');
+      li4.current.classList.remove('active_li');
+      // li3.current.classList.remove('active_li');
+      // li2.current.classList.remove('active_li');
+      // li1.current.classList.remove('active_li');
+  }
+
+  const paint_A = ( e ) => {
+
+    cleanCLass();
+
+    // if (e.currentTarget.id === "li1") {
+    //   li1.current.classList.add("active_li");
+    //   console.log(li1);
+    // } 
+    // else if (e.currentTarget.id === "li2") {
+    //   li2.current.classList.add("active_li");
+    //   console.log(li2);
+    // }
+    // else if (e.currentTarget.id === "li3") {
+    //   li3.current.classList.add("active_li");
+    //   console.log(li3);
+    // }
+    if (e.currentTarget.id === "li4") {
+      li4.current.classList.add("active_li");
+      console.log(li5);
+    }
+    else if (e.currentTarget.id === "li5") {
+      li5.current.classList.add("active_li");
+      console.log(li5);
+    }
+    else if (e.currentTarget.id === "li6") {
+      li6.current.classList.add("active_li");
+      console.log(li6);
+    }
+    // else if (e.currentTarget.id === "li7") {
+    //   li7.current.classList.add("active_li");
+    //   console.log(li7);
+    // }
+    // else if (e.currentTarget.id === "li8") {
+    //   li8.current.classList.add("active_li");
+    //   console.log(li8);
+    // }
+  }
+
   return (
     <div className='profile_nav'>
       <nav className='navi_menu_profile'>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
-        <li><Link className='a' to={""}>Mascotas</Link></li>
+        {/* <li onClick={paint_A} id="li1" ref={li1}><Link className='a' to={""}>Actualizar Mascota</Link></li>
+        <li onClick={paint_A} id="li2" ref={li2}><Link className='a' to={""}>Consultar Mascota</Link></li>
+        <li onClick={paint_A} id="li3" ref={li3}><Link className='a' to={""}>Eliminar Mascota</Link></li> */}
+        <li onClick={paint_A} id="li4" ref={li4}><Link className='a' to={""}> <i><GrDocumentUpdate /></i> Actualizar Perfil</Link></li>
+        <li onClick={paint_A} id="li5" ref={li5}><Link className='a' to={""}> <i><IoMdPaper /></i> Historias Clinicas</Link></li>
+        <li onClick={paint_A} id="li6" ref={li6}><Link className='a' to={""}> <i><MdDateRange /></i> Citas</Link></li>
+        {/* <li onClick={paint_A} id="li7" ref={li7}><Link className='a' to={""}>Mascotas Func 7</Link></li>
+        <li onClick={paint_A} id="li8" ref={li8}><Link className='a' to={""}>Mascotas Func 8</Link></li> */}
       </nav>
     </div>
   )
