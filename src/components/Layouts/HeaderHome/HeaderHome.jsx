d27c915100b2634c24bf03d2d7e251901fcb773e
JuanVera326 <jjvera86@misena.edu.co>
2022-06-18 14:52:03 -0500

    Pagina Home en Construccion

---
+++ src/components/Layouts/HeaderHome/HeaderHome.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { pets_images } from '../../../helpers/Pets_care_images';
+import { ImagenUI } from "../../UI/ImagenUI/ImagenUI";
+import './HeaderHome.css'
+
+export const Header = () => {
+  return (
+    <div className='headerHome'>
+        <div className="logo">
+          <ImagenUI img={pets_images('./home/logo.png')} style="logoHome"></ImagenUI>
+          <ImagenUI img={pets_images('./home/titulo.png')} style="tituloHome"></ImagenUI>
+        </div>
+
+        <div className="options">
+          <nav className='nav_Home'>
+            <li id="liHome"><i>Prueba la facilidad Ahora!!</i></li>
+            <li id='liHome'><Link className='aNavHome logIn' to={"/login"}>Log in!</Link></li>
+            <li id='liHome'><Link className='aNavHome signIn' to={"/registro"}>Sign up!</Link></li>
+          </nav>
+        </div>  
+    </div>
+  )
+}
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import { Header } from "../../Layouts/Header/Header"
-
+import { FooterHome } from "../../Layouts/FooterHome/FooterHome"
+import { Header } from "../../Layouts/HeaderHome/HeaderHome"
+import './Home.css'
 
 export const Home = () => {
   return (
     <div>
         <Header></Header>
-        <h1>Home</h1>
+          <div className="home">
+            <div className="sectionTop">
+              <div className="sectionLeft"></div>
+              <div className="sectionRight"></div>
+            </div>
+            <div className="sectionBottom"></div>
+          </div>
+        <FooterHome></FooterHome>
     </div>
   )
 }
