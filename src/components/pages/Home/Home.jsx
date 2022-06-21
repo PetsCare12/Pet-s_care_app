86a54078113862b86c8dc959098bad47ab4e31fe
JuanVera326 <jjvera86@misena.edu.co>
2022-06-21 17:17:41 -0500

    Construccion del Home

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
 import { FooterHome } from "../../Layouts/FooterHome/FooterHome"
 import { Header } from "../../Layouts/HeaderHome/HeaderHome"
+import { Slide } from 'react-slideshow-image';
+import { clinicas } from "./data"
+import { ImagenUI } from "../../UI/ImagenUI/ImagenUI";
+import 'react-slideshow-image/dist/styles.css'
 import './Home.css'
 
 export const Home = () => {
@@ -8,9 +12,26 @@ export const Home = () => {
         <Header></Header>
           <div className="home">
             <div className="sectionTop">
-              <div className="sectionLeft"></div>
-              <div className="sectionRight"></div>
-            </div>
+              <div className="sectionLeft">
+                <div className='slide-container'>
+                  <h3>Nuestras Clinicas Asociadas</h3>
+                      <Slide>
+                        {
+                          clinicas.map((item , index) => (
+                            <div className="each-slide" key={index}>
+                              <div>
+                                <h1>{item.id}</h1>
+                                <ImagenUI img={item.img}></ImagenUI>
+                                <h1>{item.direccion}</h1>
+                              </div>  
+                            </div>
+                          ))
+                        }
+                      </Slide>
+                    </div>
+                  </div>
+                <div className="sectionRight"></div>
+              </div>
             <div className="sectionBottom"></div>
           </div>
         <FooterHome></FooterHome>
