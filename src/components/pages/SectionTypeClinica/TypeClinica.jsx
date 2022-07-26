40e01c0bbf16aa52ad91ccf5139b5fd0ecd4a5f3
JuanVera326 <jjvera86@misena.edu.co>
2022-07-26 17:41:43 -0500

    Page Clinica Admin

--- src/components/pages/SectionTypeClinica/TypeClinica.jsx
+++ src/components/pages/SectionTypeClinica/TypeClinica.jsx
@@ -1,12 +1,72 @@
 import React from 'react'
+import { pets_images } from '../../../helpers/Pets_care_images';
 import "./TypeClinica.css";
+import { vet } from "./vetData.js"
 
 export const TypeClinica = () => {
+
+  const arr = vet;
+  console.log(arr);
   return (
     <div className='st1'>
       <div className='st2'>
           <div className='titleClin'>
-            <h1>Admisnista tu Clinica</h1>
+            <h1>Administra tu Clinica</h1>
+          </div>
+          <div className='st3'>
+            <div className='st4'>
+              <h1>Veterinarios</h1>
+              <ul>
+                {
+                  arr.map((item) =>(
+                    <li className={`liVetSpace 
+                    ${ (item.sexo === "Femenino")
+                    ? 'liVetSpaceFem' 
+                    : (item.sexo === "Masculino")
+                    ? 'liVetSpaceMas'
+                    : 'liVetSpaceInclusive'}`
+                    }>
+                      <div className='liVet'>
+                        {
+                          (item.sexo === "Masculino")
+                          ?
+                            <img src={pets_images("./veterinarios/perfil-masculino.png")} alt="Masculino" id='imgVet'/>
+                          :
+                          (item.sexo === "Femenino")
+                          ?
+                            <img src={pets_images("./veterinarios/perfil-femenino.png")} alt="Femenino" id='imgVet'/>
+                          :
+                            <img src={pets_images("./veterinarios/inclusion.png")} alt="Inclusivo" id='imgVet'/>
+                        }
+                        <div className='liVetA'>
+                          <h4>
+                            <span>Nombre: </span>
+                            {item.nombre}
+                          </h4>
+
+                          <h4>
+                            <span>Apellido: </span>
+                            {item.apellido}
+                          </h4>
+
+                          <h4>
+                            <span>Sexo: </span>
+                            {item.sexo}
+                          </h4>
+
+                          <h4>
+                            <span>Especialidad: </span>
+                            {item.especialidad}
+                          </h4>
+                        </div>
+                      </div> 
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+            <div className='st5'>
+            </div>
           </div>
       </div>
     </div>
---
+++ src/components/pages/SectionTypeClinica/vetData.js
@@ -0,0 +1,97 @@
+export const vet = [
+    {
+        documento:"00001",
+        nombre:"Kevyn",
+        apellido:"Pineda",
+        telefono:"444444",
+        correo:"pineda@gmail.com",
+        sexo:"Masculino",
+        especialidad:"Cirujano",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00002",
+        nombre:"Andres",
+        apellido:"Alvarez",
+        telefono:"55555",
+        correo:"alvarez@gmail.com",
+        sexo:"Gay",
+        especialidad:"Enfermer@ Veterinari@",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00003",
+        nombre:"Carolina",
+        apellido:"Zamora",
+        telefono:"66666",
+        correo:"zamora@gmail.com",
+        sexo:"Femenino",
+        especialidad:"Especialista Veterinaria",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00004",
+        nombre:"Nathalia",
+        apellido:"Ferrari",
+        telefono:"77777",
+        correo:"ferrari@gmail.com",
+        sexo:"Femenino",
+        especialidad:"Veterinaria Auxiliar",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00005",
+        nombre:"Leonardo",
+        apellido:"Leon",
+        telefono:"99999",
+        correo:"leon@gmail.com",
+        sexo:"Masculino",
+        especialidad:"Cirujana",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00006",
+        nombre:"Daniela",
+        apellido:"Alvarez",
+        telefono:"101010",
+        correo:"daniela@gmail.com",
+        sexo:"Femenino",
+        especialidad:"Bandida",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00007",
+        nombre:"Carlos",
+        apellido:"Pulgarin",
+        telefono:"101010",
+        correo:"carlos@gmail.com",
+        sexo:"Masculino",
+        especialidad:"Odontologo",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+    ,
+    {
+        documento:"00008",
+        nombre:"Diana",
+        apellido:"Escobar",
+        telefono:"101010",
+        correo:"diana@gmail.com",
+        sexo:"Femenino",
+        especialidad:"Farmacia",
+        imagen:"xxx",
+        clinica:"Perrito 64"
+    }
+];
