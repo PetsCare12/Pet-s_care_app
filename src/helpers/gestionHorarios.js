45860262bbe07a0769d84afb624ffc2afe1f3e39
JuanVera326 <jjvera86@misena.edu.co>
2022-08-31 13:51:41 -0500

    Tarifa agregada en registro clinica

---
+++ src/helpers/gestionHorarios.js
@@ -0,0 +1,29 @@
+
+
+export const divisionHorarios = ( ah , ch , am = 0 , cm = 0 ) => {
+
+    const day = new Date();
+
+    let horarios = [];
+    let hora = "";
+
+    const apertura = ah;
+    const cierre = 21;
+
+    day.setHours(apertura);
+    day.setMinutes(0);
+
+    const tiempoCita = 45;
+
+    hora = day.getHours()+":"+day.getMinutes();
+    horarios.push( hora );
+
+    for (let index = 0; day.getHours() < cierre; index++) {
+
+        day.setMinutes( day.getMinutes() + tiempoCita );
+        
+        hora =  day.getHours()+":"+day.getMinutes();
+        
+    }
+
+}
