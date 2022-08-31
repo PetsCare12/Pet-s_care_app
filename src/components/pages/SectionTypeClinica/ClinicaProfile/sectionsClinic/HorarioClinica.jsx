0c5c84c79f40b41dc6d110da40f337b60b3cad2c
JuanVera326 <jjvera86@misena.edu.co>
2022-08-30 20:09:54 -0500

    Seteo de horario y validacion de espacios vacios

--- src/components/pages/SectionTypeClinica/ClinicaProfile/sectionsClinic/HorarioClinica.jsx
+++ src/components/pages/SectionTypeClinica/ClinicaProfile/sectionsClinic/HorarioClinica.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { deleteHorarioGeneral, setHorarioClinica } from '../../../../../helpers/API Consumer/useHorariosConsumer';
 import { ButtonUI } from '../../../../UI/ButtonUI/ButtonUI';
 import { Dias_Horario_UI } from '../../../../UI/Dias_Horario_UI/Dias_Horario_UI';
 
 export const HorarioClinica = ( {data} ) => {
 
+  const token = localStorage.getItem('token');
+  const [requestHour, setrequestHour] = useState("");
+  
+  // useEffect(() => {
+  //   for (let index = 1; index <= 14; index++) {
+  //     deleteHorarioGeneral(index,token);
+  //   }
+  // }, [])
+  
+
   let nit = data.nit;
-  console.log(nit);
+
     const setDates = (e) => {
+
+      let errors = {};
+
         e.preventDefault();
         let hoursAvalibles = 
         [
@@ -59,8 +73,41 @@ export const HorarioClinica = ( {data} ) => {
               "horaSalida"  : e.target[13].value
             }
         ]
-    
-        console.log(hoursAvalibles);
+
+        for (let j in hoursAvalibles) {
+
+          let obj1 = hoursAvalibles[j];
+
+          if (obj1.horaInicio === "") {
+
+            errors.validacion = `Campo Vacío Hora Entrada del día ${obj1.diaHorarios}`
+            break;
+
+          }else if (obj1.horaSalida === "") {
+            
+            errors.validacion = `Campo Vacío Hora Salida del día ${obj1.diaHorarios}`
+            break;
+
+          }
+        }
+
+        if (JSON.stringify(errors) === '{}') {
+          
+          for (let k in hoursAvalibles){
+
+            let obj2 = hoursAvalibles[k];
+            setHorarioClinica( obj2 , nit , token).then( data => {
+              console.log(data);
+            });
+
+          }
+
+        }else{
+
+          console.log("No se puede registrar el horario");
+          console.log(errors.validacion);
+
+        }
       }
 
   return (
@@ -75,18 +122,21 @@ export const HorarioClinica = ( {data} ) => {
           dia={"Lunes"}
           name_hour_in={"lunes_in"}
           name_hour_out={"lunes_out"}
+          value={""}
         />
 
         <Dias_Horario_UI 
           dia={"Martes"}
           name_hour_in={"martes_in"}
           name_hour_out={"martes_out"}
+          value={""}
         />
   
         <Dias_Horario_UI 
           dia={"Miercoles"}
           name_hour_in={"martes_in"}
           name_hour_out={"martes_out"}
+          value={""}
         />
         
       </div>
@@ -97,24 +147,28 @@ export const HorarioClinica = ( {data} ) => {
           dia={"Jueves"}
           name_hour_in={"jueves_in"}
           name_hour_out={"jueves_out"}
+          value={""}
         />
 
         <Dias_Horario_UI 
           dia={"Viernes"}
           name_hour_in={"viernes_in"}
           name_hour_out={"viernes_out"}
+          value={""}
         />
 
         <Dias_Horario_UI 
           dia={"Sabado"}
           name_hour_in={"sabado_in"}
           name_hour_out={"sabado_out"}
+          value={""}
         />
 
         <Dias_Horario_UI 
           dia={"Domingo"}
           name_hour_in={"domingo_in"}
           name_hour_out={"domingo_out"}
+          value={""}
         />
 
      </div>
