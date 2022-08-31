1f5fc18ea2c2a72db64684fcd89f49f5926674f0
JuanVera326 <jjvera86@misena.edu.co>
2022-08-31 10:27:59 -0500

    Correcciones de validaciones de formulrios

--- src/components/pages/SectionTypeClinica/ModalRegisterVet.jsx
+++ src/components/pages/SectionTypeClinica/ModalRegisterVet.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { MdOutlineCancel } from 'react-icons/md';
 import { useSendImage } from '../../../helpers/Cloudinary_Images/useSendImages';
 import { ButtonUI } from '../../UI/ButtonUI/ButtonUI';
-import { useForm } from '../../../helpers/useForm';
 import { imageRandom } from '../../../helpers/RandomImages/imagenessa';
-import { getVeterinarioById } from '../../../helpers/API Consumer/useVeterinariosConsumer';
-import { useRef } from 'react';
+import { getVeterinarioById, setVeterinario } from '../../../helpers/API Consumer/useVeterinariosConsumer';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { AiFillCheckCircle } from 'react-icons/ai';
 import "./ModalRegisterVet.css";
 
-export const ModalRegisterVet = ({ children , isOpen , closeModal , token , nit }) => {
+export const ModalRegisterVet = ({ isOpen , closeModal , token , nit }) => {
 
   let imgDefault = imageRandom();
 
@@ -16,92 +16,70 @@ export const ModalRegisterVet = ({ children , isOpen , closeModal , token , nit 
   const {myWidgetVeter,urlImage} = useSendImage();
   const [imgUrl, setimg] = useState(imgDefault);
 
-  // const form_vet = useRef(null);
-  
-  // const handleSubmitForm_vet = (e) => {
-
-  //   const formData = new FormData(form_vet.current);
-
-  //   const data = {
-
-  //     documento: formData.get('documento'),
-  //     nombre: formData.get('nombre'),
-  //     apellidos: formData.get('apellidos'),
-  //     sexovt: formData.get('sexovt'),
-  //     telefono: formData.get('telefono'),
-  //     correo: formData.get('correo'),
-  //     especialidad: formData.get('especialidad'),
-  //     password: formData.get('password'),
-  //     imagenVete: "",
-  //     estadoVt: formData.get('estadoVt')
-
-  //   }
-  // }
-
+  const [serverError, setServerError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [duplicatedData, setDuplicatedData] = useState(false);
+  const [registered, setRegistered] = useState(false);
 
 
   //////////////////////////////////////////////////////
 
-  const initialForm = {
-    documento: "",
-    nombre: "",
-    apellidos: "",
-    sexovt: "none",
-    telefono: "",
-    correo: "",
-    especialidad: "",
-    password: "",
-    imagenVete: "",
-    estadoVt: 0
-  };
+  // const initialForm = {
+  //   documento: "",
+  //   nombre: "",
+  //   apellidos: "",
+  //   sexovt: "none",
+  //   telefono: "",
+  //   correo: "",
+  //   especialidad: "",
+  //   password: "",
+  //   imagenVete: "",
+  //   estadoVt: 0
+  // };
 
-  const validationsForm = (form) => {
+  // const validationsForm = (form) => {
 
-    getVeterinarioById(form.documento).then(data => { 
+  //   getVeterinarioById(form.documento).then(data => { 
 
-      if (data.status === 404) {
-        setval_exist(404);
-        console.log(setval_exist);
-      }else{
-        setval_exist(200); 
-      }
+  //     if (data.status === 404) {
+  //       setval_exist(404);
+  //       console.log(setval_exist);
+  //     }else{
+  //       setval_exist(200); 
+  //     }
         
-    });
+  //   });
 
-    let errors = {};
+  //   // else if ( val_exist !== 404 ) { errors.documento = "El documento ya existe" }
+  //   // let errors = {};
 
-        // if (!form.documento.trim() 
-        // // || !/^\d{7,}$/.test(form.documento)
-        // ) { errors.documento = "Documento erroneo mín. 7 caracteres y solo números" }
-        
-    // else 
-    if ( val_exist !== 404 ) { errors.documento = "El documento ya existe" }
+  //   //     if (!form.documento.trim()  || !/^\d{7,}$/.test(form.documento)) { errors.documento = "Documento erroneo mín. 7 caracteres y solo números" }
 
-    else if (!form.nombre.trim()) { errors.nombre = "Nombres erroneos" }
-    else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.nombre)) { errors.nombre = "Nombres erroneos" }
+  //   // else if (!form.nombre.trim()) { errors.nombre = "Nombres erroneos" }
+  //   // else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.nombre)) { errors.nombre = "Nombres erroneos" }
 
-    else if (!form.apellidos.trim()) { errors.apellidos = "Apellidos erroneos" }
-    else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.apellidos)) { errors.apellidos = "Apellidos erroneos" }
+  //   // else if (!form.apellidos.trim()) { errors.apellidos = "Apellidos erroneos" }
+  //   // else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.apellidos)) { errors.apellidos = "Apellidos erroneos" }
 
-    else if (!form.telefono.trim()) { errors.telefono = "Telefono erroneo" }
-    else if (!/^\d{10,10}$/.test(form.telefono)) { errors.telefono = "Telefono erroneo mín. 10 caracteres y solo números" }
+  //   // else if (!form.telefono.trim()) { errors.telefono = "Telefono erroneo" }
+  //   // else if (!/^\d{10,10}$/.test(form.telefono)) { errors.telefono = "Telefono erroneo mín. 10 caracteres y solo números" }
 
-    else if (!form.sexovt === "none") {errors.sexovt = "Sexo requerido"}
-    else if (!form.estadoVt === 0) {errors.estadoVt = "Estado requerido"}
+  //   // else if (!form.sexovt === "none") {errors.sexovt = "Sexo requerido"}
+  //   // else if (!form.estadoVt === 0) {errors.estadoVt = "Estado requerido"}
 
-    else if (!form.correo.trim()) { errors.correo = "Correo erroneo" }
-    else if (!/^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/.test(form.correo)) { errors.correo = "Correo erroneo" }
+  //   // else if (!form.correo.trim()) { errors.correo = "Correo erroneo" }
+  //   // else if (!/^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/.test(form.correo)) { errors.correo = "Correo erroneo" }
 
-    else if (!form.especialidad.trim()) { errors.especialidad = "Especialidad erronea" }
-    else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.especialidad)) { errors.especialidad = "Especialidad erronea" }
+  //   // else if (!form.especialidad.trim()) { errors.especialidad = "Especialidad erronea" }
+  //   // else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(form.especialidad)) { errors.especialidad = "Especialidad erronea" }
 
-    else if (!form.password.trim()) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
-    else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/.test(form.password)) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
+  //   // else if (!form.password.trim()) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
+  //   // else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/.test(form.password)) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
 
 
-      return errors;
-  }
-  const {form,errors,loading,response,estatusResponse,handleChangeVet,handleBlur,handleSubmit} = useForm(initialForm,validationsForm,token,nit);
+  //     return errors;
+  // }
+  // const {form,errors,loading,response,estatusResponse,handleChangeVet,handleBlur,handleSubmit} = useForm(initialForm,validationsForm,token,nit);
 
   const showWidget = () => {myWidgetVeter.open();};
 
@@ -122,13 +100,187 @@ export const ModalRegisterVet = ({ children , isOpen , closeModal , token , nit 
 
           <h2>{"Registra un Veterinario"}</h2>
 
-            <form onSubmit={handleSubmit} ref={form_vet} className='formVet_register animate__animated animate__fadeIn'>
+          <Formik
+
+            initialValues={{
+              documento: "",
+              nombre: "",
+              apellidos: "",
+              sexovt: "none",
+              telefono: "",
+              correo: "",
+              especialidad: "",
+              password: "",
+              imagenVete: "",
+              estadoVt: 0
+            }}
+
+            validate = {(valores) => {
+              let errors = {};
+
+              if (!valores.documento.trim()  || !/^\d{7,}$/.test(valores.documento)) { errors.documento = "Documento erroneo mín. 7 caracteres y solo números" }
+      
+              else if (!valores.nombre.trim()) { errors.nombre = "Nombres erroneos" }
+              else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(valores.nombre)) { errors.nombre = "Nombres erroneos" }
+          
+              else if (!valores.apellidos.trim()) { errors.apellidos = "Apellidos erroneos" }
+              else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(valores.apellidos)) { errors.apellidos = "Apellidos erroneos" }
+          
+              else if (!valores.telefono.trim()) { errors.telefono = "Telefono erroneo" }
+              else if (!/^\d{10,10}$/.test(valores.telefono)) { errors.telefono = "Telefono erroneo mín. 10 caracteres y solo números" }
+          
+              else if (!valores.sexovt === "none") {errors.sexovt = "Sexo requerido"}
+              else if (!valores.estadoVt === 0) {errors.estadoVt = "Estado requerido"}
+          
+              else if (!valores.correo.trim()) { errors.correo = "Correo erroneo" }
+              else if (!/^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/.test(valores.correo)) { errors.correo = "Correo erroneo" }
+          
+              else if (!valores.especialidad.trim()) { errors.especialidad = "Especialidad erronea" }
+              else if (!/^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/.test(valores.especialidad)) { errors.especialidad = "Especialidad erronea" }
+          
+              else if (!valores.password.trim()) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
+              else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/.test(valores.password)) {errors.password = "Debes incluir minúsculas, mayúsculas, números y caracteres especiales en la contraseña"}
+              
+              return errors;
+            }}
+
+            onSubmit={( valores, {resetForm} ) => {
+
+              // const img = imageRandom();
+              // valores.imagenVete = img;
+
+              let validacion = {};
+              setVeterinario( valores , nit , token ).then(info => {
+                info = validacion;
+
+                setLoading(true);
+                if ( validacion.status === 400 ) {
+                    setDuplicatedData( true );
+                    setServerError( false );
+                    setLoading(false);
+                }
+                else if( validacion.status === 500 ){
+                    setServerError( true );
+                    setDuplicatedData( false );
+                    setLoading( false );
+                }
+                else {
+                    setDuplicatedData( false );
+                    resetForm();
+                    setLoading(false);
+                    setRegistered( true );
+                }
+              })
+
+          }}>
+                 {({ errors }) => (
+                    <Form>
+                        {
+                            ( duplicatedData ) &&
+                            <p id='registerUser__error'>Al parecer tu correo o documento ya están registrados</p>
+                        }
+                        {
+                            ( serverError ) &&
+                            <p id='registerUser__error'>Hubo un error en el registro, intentalo nuevamente.</p>
+                        }
+                        <Field 
+                            type='text'
+                            placeholder='Documento' 
+                            className='input regs' 
+                            name='documento'
+                            id="documento" 
+                        />
+                        <ErrorMessage name='documentoUs' component={() => (<p className='warn__password-user'>{errors.documento}</p>)} />
+
+                            <Field 
+                                type='text'
+                                placeholder='Nombre' 
+                                className='input regs'
+                                name='nombre'
+                                id="nombre"
+                            />
+                            <Field 
+                                type='text'
+                                placeholder='Apellido' 
+                                className='input regs'
+                                name='apellidos'
+                                id="apellidos"
+                                />
+
+                        <ErrorMessage name='nombreUs' component={()   => (<p className='warn__password-user' style={{textAlign:"left"}}>{errors.nombre}</p>)} />
+                        <ErrorMessage name='apellidoUs' component={() => (<p className='warn__password-user' style={{textAlign:"right"}}>{errors.apellidos}</p>)} />
+
+                        <Field 
+                            type='text'
+                            placeholder='Correo' 
+                            className='input regs'
+                            name='correo'
+                            id="correo"
+                        />
+                        <ErrorMessage name='correoUs' component={() => (<p className='warn__password-user'>{errors.correo}</p>)} />
+
+            
+                        <Field 
+                            type='text'
+                            placeholder='Telefono' 
+                            className='input regs'
+                            name='telefono'
+                            id="telefono"
+                        />
+                        <Field name="sexovt" id="sexovt" className='input selects' as="select">
+                            <option value="none">Selecciona el sexo</option>
+                            <option value="hombre">Hombre</option>
+                            <option value="mujer">Mujer</option>
+                            <option value="otro">Otro</option>
+                        </Field>
+                        
+                        <Field name="estadoVt" id="estadoVt" className='input selects'>
+                            <option value="0">Seleccione un estado</option>
+                            <option value="1">Activo</option>
+                            <option value="2">Inactivo</option>
+                        </Field>
+
+                        <ErrorMessage name='telefonoUs' component={()   => (<p className='warn__password-user' style={{textAlign:"left"}}>{errors.telefono}</p>)} />
+                        <ErrorMessage name='sexoUs' component={() => (<p className='warn__password-user' style={{textAlign:"right"}}>{errors.sexovt}</p>)} />
+
+                        <Field
+                            type="text" 
+                            placeholder='Especialidad' 
+                            className='input regs'
+                            name='especialidad'
+                            id='especialidad'
+                        />  
+                        
+                        <Field 
+                            type='password'
+                            placeholder='Contraseña' 
+                            className='input'
+                            name='password'
+                            id="password"
+                        />
+                        <ErrorMessage name='passwordUs' component={() => (<p className='warn__password-user'>{errors.password}</p>)} />
+                        <ButtonUI 
+                            text="Registrar"
+                            style={`btnLogin ${( loading )? 'hidden' :( registered )? 'hidden' : ""}`}
+                            type={"submit"}
+                        />
+                        {
+                            ( loading ) && <div className='register__loading-div'><div className='spiner' id='login-spin'></div></div>
+                        }
+                        {
+                            ( registered ) && <div className='registerUser__registered'><AiFillCheckCircle style={{fontSize:"45px",color:"#00c600"}}/><p>¡Registro exitoso!</p></div>
+                        }
+                    </Form>
+                )}
+          </Formik>
+
+            {/* <form onSubmit={handleSubmit} ref={form_vet} className='formVet_register animate__animated animate__fadeIn'>
 
                 {loading && <div id='login-spin-clinic' className='spiner'></div>}
                 {response && <p id='succesP  animate__animated animate__fadeIn'>{estatusResponse}</p>}
 
               <div title='Sube una Imagen!' className="img_cont_vet" onClick={showWidget}>
-                <img src={form.imagenVete = imgUrl} alt="" className='imgForm'/>
+                <img src={form.imagenVete = imgUrl} name="imagenVete" alt="" className='imgForm'/>
               </div>
 
               <div className="parts part1">
@@ -239,7 +391,7 @@ export const ModalRegisterVet = ({ children , isOpen , closeModal , token , nit 
 
               <ButtonUI text="Registrar Veterinario"  type="submit" style="submit vetRes"></ButtonUI>
 
-            </form>
+            </form> */}
           </div>
         </div>
     </div>
